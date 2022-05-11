2a64379208e59f4208d2a4dfc97c3773a3e67871
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-05-10 21:34:53 -0300

    refactor: remove username column from user table

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -13,7 +13,6 @@ class CreateUserUseCase {
   async execute(data: ICreateUserDTO): Promise<void> {
     await this.usersRepository.create({
       name: data.name,
-      username: data.username,
       email: data.email,
       password: data.password,
       driver_license: data.driver_license,
