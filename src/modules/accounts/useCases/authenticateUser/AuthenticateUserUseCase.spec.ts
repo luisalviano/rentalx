cd3c96af3ec23263f7abdf135c5404b3082f0589
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-05-18 21:10:31 -0300

    refactor: update imports using tsconfig-paths

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,7 +1,7 @@
-import { ICreateUserDTO } from "modules/accounts/dtos/ICreateUserDTO";
+import { AppError } from "@errors/AppError";
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
 
-import { AppError } from "../../../../errors/AppError";
-import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
